3f356bef77a41df561bb943883d0a67684d60985
ahbab1995 <islamahbab1996@yahoo.com>
2022-04-06 02:22:53 +0600

    api project new code add

--- js/app.js
+++ js/app.js
@@ -1,1 +1,13 @@
-alert()
+
+const playerSearch = () => {
+      const searchValue =  document.getElementById('searchInput').value
+      const url = `https://www.thesportsdb.com/api/v1/json/2/searchplayers.php?p=${searchValue}`
+      fetch(url)
+      .then(res => res.json())
+      .then(data => showTeams(data.player))
+    //   console.log(url) 
+}
+
+function showTeams(teams) {
+    console.log(teams)
+}
